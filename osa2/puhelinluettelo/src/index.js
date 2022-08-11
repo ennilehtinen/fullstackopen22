9bbb0a4feb31b3ff6c58eb54fb62de477486926a
ennilehtinen <ennilehtinen9@gmail.com>
2022-08-11 15:10:23 +0300

    puhelinluettelo 2.9 lis. ihmisiä

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -8,6 +8,16 @@ const persons = [
     id: 1,
     name: 'Kari Hellas',
     number: '0401231244',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '0401231245',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '0401231246',
   }
 ]
 
