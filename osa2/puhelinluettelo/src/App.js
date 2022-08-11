63d2eaebee9c92d8920a6a343b55b5a1543c4064
ennilehtinen <ennilehtinen9@gmail.com>
2022-08-11 15:06:42 +0300

    puhelinluettelo 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,6 +6,7 @@ const App = (props) => {
   const [persons, setPersons] = useState(props.persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setNewSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -34,9 +35,22 @@ const App = (props) => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setNewSearch(event.target.value)
+  }
+
+  const filtered = !search
+    ? persons
+    : persons.filter((person) =>
+      person.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Filter shown with <input value={search} onChange={handleSearchChange} /></div>
+      <div><button type="filter">filter</button></div>
+      <h2>Add new contact</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
@@ -44,7 +58,7 @@ const App = (props) => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {filtered.map(person =>
           <Person key={person.id} person={person} />
         )}
       </ul>
